b8ce73afd8fb6d773616218f27ea381a9bdb05eb
Zicr0n <alwanisamir007@gmail.com>
2025-03-31 22:25:48 +0200

    installation_2

--- src/lib/Components/App.svelte
+++ src/lib/Components/App.svelte
@@ -7,7 +7,7 @@
 
     // https://dev.to/atapas/the-best-way-to-get-the-month-name-from-a-date-in-javascript-3pjn
     let month = date.toLocaleString('default', {month : 'long'})
-    
+
     let year = date.getFullYear()
 </script>
 
@@ -21,7 +21,7 @@
 </main>
 
 <nav class="fixed w-full h-32 flex items-center px-16 gap-16">
-    <p class="">Alalwani<br>Mohammad<br>Samir</p>
+    <p class="">Alalwani<br>Mohammad<br>Samirs</p>
     <p>{day} {month} <br>{year}</p>
 </nav>
 
