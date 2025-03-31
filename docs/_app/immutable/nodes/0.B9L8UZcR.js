a5e483042d9e282efa3cf2b98bfc65595f35c571
Zicr0n <alwanisamir007@gmail.com>
2025-03-31 22:39:53 +0200

    installation_6

---
+++ docs/_app/immutable/nodes/0.B9L8UZcR.js
@@ -0,0 +1,1 @@
+import{c as n,b as a}from"../chunks/x9kSB0vh.js";import{Q as p}from"../chunks/DEZjJdoi.js";import{s}from"../chunks/BXE_77Sr.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,29 @@
 import adapter from '@sveltejs/adapter-static';
 
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), ],
+	extensions: ['.svelte'],
+
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			pages: 'docs',
+			assets: 'docs',
+			fallback: "404.html"
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+        paths:{
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        }
+		
+	},
 };
 
 export default config;
+			
+			
